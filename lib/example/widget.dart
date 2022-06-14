b916d6e78ead89b41944e2558700205ebe566bc6
hdinjos <hdinjos@gmail.com>
2022-06-14 15:01:37 +0700

    make widget

---
+++ lib/example/widget.dart
@@ -0,0 +1,64 @@
+import "package:flutter/material.dart";
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Flutter Demo",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: Scaffold(
+          appBar: AppBar(
+            title: const Text("Halo dunia"),
+          ),
+          body: const Center(
+            child: BiggerText(text: "Test yaa..."),
+          )),
+    );
+  }
+}
+
+class Heading extends StatelessWidget {
+  final String text;
+  const Heading({Key? key, required this.text}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(text,
+        style: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold));
+  }
+}
+
+class BiggerText extends StatefulWidget {
+  final String text;
+  const BiggerText({Key? key, required this.text}) : super(key: key);
+
+  @override
+  State<BiggerText> createState() => _BiggerTextState();
+}
+
+class _BiggerTextState extends State<BiggerText> {
+  double _textSize = 16.0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(widget.text, style: TextStyle(fontSize: _textSize)),
+        ElevatedButton(
+            onPressed: () {
+              setState(() {
+                _textSize = 30.0;
+              });
+            },
+            child: const Text("Perbesar"))
+      ],
+    );
+  }
+}
