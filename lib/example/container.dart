a0d14038c55447496fe851c9928d6216b3e5df0c
hdinjos <hdinjos@gmail.com>
2022-06-15 11:33:59 +0700

    padding wiget

--- lib/example/container.dart
+++ lib/example/container.dart
@@ -34,10 +34,25 @@ class FirstScreen extends StatelessWidget {
               onPressed: () {},
             )
           ]),
-      body: const Padding(
-        padding: EdgeInsets.all(20),
-        child: Text("HALO GAIS"),
-      ),
+      body: Container(
+          // color: Colors.blue,
+          width: 200,
+          height: 200,
+          padding: const EdgeInsets.all(10),
+          margin: const EdgeInsets.all(10),
+          transform: Matrix4.rotationZ(0.1),
+          decoration: BoxDecoration(
+              color: Colors.red,
+              // shape: BoxShape.circle,
+              border: Border.all(color: Colors.green, width: 3),
+              borderRadius: BorderRadius.circular(10),
+              boxShadow: const [
+                BoxShadow(
+                    color: Colors.black, offset: Offset(3, 6), blurRadius: 10)
+              ]),
+          child: const Text("H", style: TextStyle(fontSize: 30))),
+      floatingActionButton:
+          FloatingActionButton(child: const Icon(Icons.add), onPressed: () {}),
     );
   }
 }
